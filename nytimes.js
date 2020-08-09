213eac2cd256142a3363eed2325d7882217e82fe
ribm <kr151ranjan@gmail.com>
2020-08-09 09:31:11 +0530

    Add files via upload
    committed

---
+++ nytimes.js
@@ -0,0 +1,285 @@
+var k = {
+    "status": "OK",
+    "copyright": "Copyright (c) 2020 The New York Times Company. All Rights Reserved.",
+    "section": "Books",
+    "last_updated": "2020-08-07T14:09:08-04:00",
+    "num_results": 34,
+    "results": [
+      
+      {
+        "section": "books",
+        "subsection": "review",
+        "title": "Why the Mueller Investigation Failed",
+        "abstract": "Jeffrey Toobin’s “True Crimes and Misdemeanors” examines the battle over Robert Mueller’s report and how President Trump prevailed.",
+        "url": "https://www.nytimes.com/2020/08/05/books/review/true-crimes-and-misdemeanors-jeffrey-toobin.html",
+        "uri": "nyt://article/5d70247c-09d3-5289-9d30-c7ce9f5de793",
+        "byline": "By Katie Benner",
+        "item_type": "Article",
+        "updated_date": "2020-08-06T08:55:42-04:00",
+        "created_date": "2020-08-05T13:00:04-04:00",
+        "published_date": "2020-08-05T13:00:04-04:00",
+        "material_type_facet": "",
+        "kicker": "nonfiction",
+        "des_facet": [
+          "Books and Literature",
+          "Russian Interference in 2016 US Elections and Ties to Trump Associates",
+          "Presidential Election of 2016",
+          "Special Prosecutors (Independent Counsel)",
+          "True Crimes and Misdemeanors (Book)"
+        ],
+        "org_facet": [],
+        "per_facet": [
+          "Mueller, Robert S III",
+          "Toobin, Jeffrey",
+          "Trump, Donald J"
+        ],
+        "geo_facet": [],
+        "multimedia": [
+          {
+            "url": "https://static01.nyt.com/images/2020/08/23/books/review/Benner/merlin_155598825_f87418af-f13a-454c-9d9b-59b499dd9a45-superJumbo.jpg",
+            "format": "superJumbo",
+            "height": 1365,
+            "width": 2048,
+            "type": "image",
+            "subtype": "photo",
+            "caption": "Robert Mueller as seen on television screens in New York City’s Times Square in 2019.",
+            "copyright": "Damon Winter/The New York Times"
+          },
+          {
+            "url": "https://static01.nyt.com/images/2020/08/23/books/review/Benner/Benner-thumbStandard.jpg",
+            "format": "Standard Thumbnail",
+            "height": 75,
+            "width": 75,
+            "type": "image",
+            "subtype": "photo",
+            "caption": "Robert Mueller as seen on television screens in New York City’s Times Square in 2019.",
+            "copyright": "Damon Winter/The New York Times"
+          },
+          {
+            "url": "https://static01.nyt.com/images/2020/08/23/books/review/Benner/Benner-thumbLarge.jpg",
+            "format": "thumbLarge",
+            "height": 150,
+            "width": 150,
+            "type": "image",
+            "subtype": "photo",
+            "caption": "Robert Mueller as seen on television screens in New York City’s Times Square in 2019.",
+            "copyright": "Damon Winter/The New York Times"
+          },
+          {
+            "url": "https://static01.nyt.com/images/2020/08/23/books/review/Benner/merlin_155598825_f87418af-f13a-454c-9d9b-59b499dd9a45-mediumThreeByTwo210.jpg",
+            "format": "mediumThreeByTwo210",
+            "height": 140,
+            "width": 210,
+            "type": "image",
+            "subtype": "photo",
+            "caption": "Robert Mueller as seen on television screens in New York City’s Times Square in 2019.",
+            "copyright": "Damon Winter/The New York Times"
+          },
+          {
+            "url": "https://static01.nyt.com/images/2020/08/23/books/review/Benner/merlin_155598825_f87418af-f13a-454c-9d9b-59b499dd9a45-articleInline.jpg",
+            "format": "Normal",
+            "height": 127,
+            "width": 190,
+            "type": "image",
+            "subtype": "photo",
+            "caption": "Robert Mueller as seen on television screens in New York City’s Times Square in 2019.",
+            "copyright": "Damon Winter/The New York Times"
+          }
+        ],
+        "short_url": "https://nyti.ms/2Pr7wUP"
+      },
+      {
+        "section": "books",
+        "subsection": "review",
+        "title": "‘The Death of Vivek Oji,’ by Akwaeke Emezi: An Excerpt",
+        "abstract": "An excerpt from “The Death of Vivek Oji,” by Akwaeke Emezi",
+        "url": "https://www.nytimes.com/2020/08/05/books/review/the-death-of-vivek-oji-by-akwaeke-emezi-an-excerpt.html",
+        "uri": "nyt://article/2c4d20c3-e83e-58ae-80db-cb8777faaec4",
+        "byline": "",
+        "item_type": "Article",
+        "updated_date": "2020-08-05T06:23:03-04:00",
+        "created_date": "2020-08-05T06:23:03-04:00",
+        "published_date": "2020-08-05T06:23:03-04:00",
+        "material_type_facet": "",
+        "kicker": "",
+        "des_facet": [],
+        "org_facet": [],
+        "per_facet": [],
+        "geo_facet": [],
+        "multimedia": null,
+        "short_url": "https://nyti.ms/2C3LfJI"
+      },
+      {
+        "section": "books",
+        "subsection": "review",
+        "title": "Hit Hard by Recession, an Irish Family Faces a Bitter Struggle",
+        "abstract": "Caoilinn Hughes’s novel “The Wild Laughter” considers the catastrophic effects of the fall of the Celtic Tiger.",
+        "url": "https://www.nytimes.com/2020/08/05/books/review/wild-laughter-caoilinn-hughes.html",
+        "uri": "nyt://article/f88a39e9-133a-5c6a-82bf-c0bc999ff61e",
+        "byline": "By John Boyne",
+        "item_type": "Article",
+        "updated_date": "2020-08-05T17:30:46-04:00",
+        "created_date": "2020-08-05T05:00:06-04:00",
+        "published_date": "2020-08-05T05:00:06-04:00",
+        "material_type_facet": "",
+        "kicker": "Fiction",
+        "des_facet": [
+          "Books and Literature",
+          "The Wild Laughter (Book)"
+        ],
+        "org_facet": [],
+        "per_facet": [
+          "Hughes, Caoilinn"
+        ],
+        "geo_facet": [],
+        "multimedia": [
+          {
+            "url": "https://static01.nyt.com/images/2020/06/03/books/review/Boyne1/Boyne1-superJumbo.jpg",
+            "format": "superJumbo",
+            "height": 1264,
+            "width": 1469,
+            "type": "image",
+            "subtype": "photo",
+            "caption": "Caoilinn Hughes",
+            "copyright": "Danijel Mihajlović\n"
+          },
+          {
+            "url": "https://static01.nyt.com/images/2020/06/03/books/review/Boyne1/Boyne1-thumbStandard.jpg",
+            "format": "Standard Thumbnail",
+            "height": 75,
+            "width": 75,
+            "type": "image",
+            "subtype": "photo",
+            "caption": "Caoilinn Hughes",
+            "copyright": "Danijel Mihajlović\n"
+          },
+          {
+            "url": "https://static01.nyt.com/images/2020/06/03/books/review/Boyne1/Boyne1-thumbLarge.jpg",
+            "format": "thumbLarge",
+            "height": 150,
+            "width": 150,
+            "type": "image",
+            "subtype": "photo",
+            "caption": "Caoilinn Hughes",
+            "copyright": "Danijel Mihajlović\n"
+          },
+          {
+            "url": "https://static01.nyt.com/images/2020/06/03/books/review/Boyne1/Boyne1-mediumThreeByTwo210.jpg",
+            "format": "mediumThreeByTwo210",
+            "height": 140,
+            "width": 210,
+            "type": "image",
+            "subtype": "photo",
+            "caption": "Caoilinn Hughes",
+            "copyright": "Danijel Mihajlović\n"
+          },
+          {
+            "url": "https://static01.nyt.com/images/2020/06/03/books/review/Boyne1/Boyne1-articleInline.jpg",
+            "format": "Normal",
+            "height": 163,
+            "width": 190,
+            "type": "image",
+            "subtype": "photo",
+            "caption": "Caoilinn Hughes",
+            "copyright": "Danijel Mihajlović\n"
+          }
+        ],
+        "short_url": "https://nyti.ms/3fBC01a"
+      }
+    ]
+  }
+
+  //var id  = document.getElementById("books")
+
+  function repeat(id){
+      //document.querySelectorAll(".card mb-3 collapse").forEach(e=>e.parentNode.removeChild(e));
+       //var j = temp(id)
+       search(id)
+       
+       
+      
+  }
+
+var count = 0
+function render(n,data){
+  count++
+  console.log(count)
+    var k = document.createElement("div");
+    k.className="card mb-3 collapse"
+    k.id="rest1"
+    k.setAttribute("style","max-width: 540px;")
+    k.setAttribute("style","float: left;")
+    k.innerHTML="ranjan"
+    var l = document.createElement("div")
+    l.className="row no-gutters"
+    l.id="rest1"
+    l.innerHTML="yhd"
+    k.appendChild(l)
+    var m = document.createElement("div");
+    m.className="col md-8"
+    var n = document.createElement("div");
+    n.className="col md-4"
+    l.appendChild(m)
+    l.appendChild(n)
+    var o = document.createElement("div");
+    o.className="card-body"
+    m.appendChild(o)
+    var p = document.createElement("div");
+    p.className="card-title"
+    o.appendChild(p)
+    var q = document.createElement("div");
+    q.className="card-text"
+    q.innerHTML="hiiiii"
+    o.appendChild(q)
+    var r = document.createElement("div");
+    r.className="card-text"
+    r.innerHTML="hiiiiirrr"
+    var s = document.createElement("div");
+    s.className="card-text"
+    s.innerHTML="hiiiiirrr1111"
+    o.appendChild(s)
+    
+
+
+    var t = document.createElement("img");
+    t.className="card-image"
+    t.setAttribute("src", "https://static01.nyt.com/images/2020/08/23/books/review/Benner/merlin_155598825_f87418af-f13a-454c-9d9b-59b499dd9a45-articleInline.jpg")
+    n.appendChild(t)
+    
+
+    document.body.appendChild(k)
+
+
+
+
+}
+
+//fetcing newyork times
+  async function search(id){
+      
+       
+       
+  try{
+      var response = await fetch("https://api.nytimes.com/svc/topstories/v2/"+id+".json?api-key=X8qN7UAzOVXAoHhoYc5DvGG68Gc5nhgl")
+      var result = await response.json()
+      console.log(result)
+      //alert("Movie Casting...."+result.Actors+"Movie Year..."+result.Year)
+      for (let index = 0; index < result.results.length; index++) {
+        var lp = ""
+        
+        render(lp,result)
+        console.log("inside"+" "+index)
+        
+    }
+  
+  
+  
+  }
+  catch(error){
+      console.log(error)
+  }
+      
+  
+      
+  
+  }
